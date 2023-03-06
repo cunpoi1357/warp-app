678e0cc423dce3997a3e117b7d66111ef01fc234
cunpoi <cunpoi1357@gmail.com>
2023-03-06 23:19:18 +0700

    fix config

--- app/api/thu-tu/route.ts
+++ app/api/thu-tu/route.ts
@@ -14,8 +14,8 @@ export async function GET() {
     if (!data) {
         data = await prisma.config.create({
             data: {
-                id: '[{"name":"Thông báo đầu","id":"tbd","hidden":"false"},{"name":"Thông báo","id":"tb","hidden":"false"},{"name":"Khu vực PB mod ninja school","id":"kvpbmns","hidden":"false"},{"name":"Khu vực bán vé tự động","id":"kvbvltd","hidden":"false"},{"name":"Khu vực bán nick ninja school","id":"kvbnns","hidden":"false"},{"name":"Bản hot mới cập nhật","id":"bhmcn","hidden":"false"},{"name":"Khu vực tải giả lập","id":"kvtgl","hidden":"false"},{"name":"Bán tool, mod pb theo yêu cầu","id":"btmpbntyc","hidden":"false"},{"name":"Khu vực bán xu tự động ","id":"kvbxtd","hidden":"false"},{"name":"Top ","id":"top","hidden":"false"}]',
-                value: ''
+                id: 'thu-tu',
+                value: '[{"name":"Thông báo đầu","id":"tbd","hidden":"false"},{"name":"Thông báo","id":"tb","hidden":"false"},{"name":"Khu vực PB mod ninja school","id":"kvpbmns","hidden":"false"},{"name":"Khu vực bán vé tự động","id":"kvbvltd","hidden":"false"},{"name":"Khu vực bán nick ninja school","id":"kvbnns","hidden":"false"},{"name":"Bản hot mới cập nhật","id":"bhmcn","hidden":"false"},{"name":"Khu vực tải giả lập","id":"kvtgl","hidden":"false"},{"name":"Bán tool, mod pb theo yêu cầu","id":"btmpbntyc","hidden":"false"},{"name":"Khu vực bán xu tự động ","id":"kvbxtd","hidden":"false"},{"name":"Top ","id":"top","hidden":"false"}]'
             }
         })
     }
