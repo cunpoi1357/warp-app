b2240875f8e604534d15b5c3ad0ca9671af8b993
cunpoi <cunpoi1357@gmail.com>
2023-03-07 21:21:58 +0700

    fix localStorage

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -12,7 +12,30 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio'
 
 import Input from '~/app/components/Input'
 import { auth } from '~/app/libs/firebase'
-import { getWithExpiry, setWithExpiry } from '~/app/libs/localStorage'
+function setWithExpiry(key: string, value: string, ttl: number) {
+    const now = new Date()
+
+    const item = {
+        value: value,
+        expiry: now.getTime() + ttl
+    }
+    window.localStorage.setItem(key, JSON.stringify(item))
+}
+
+function getWithExpiry(key: string) {
+    const itemStr = window.localStorage.getItem(key)
+    if (!itemStr) {
+        return null
+    }
+    const item = JSON.parse(itemStr)
+    const now = new Date()
+
+    if (now.getTime() > item.expiry) {
+        window.localStorage.removeItem(key)
+        return null
+    }
+    return item.value
+}
 
 function LoginPage() {
     const { control, handleSubmit, getValues, setValue } = useForm()
